c067db3944c346ee35955a5e5d9d1c8dd238b7b0
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-10-27 17:02:31 -0700

    Add files via upload

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/printing.py
@@ -0,0 +1,7 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this")
+print("print what you want to print")
+
+print("Hellow world!\nHellow world\nHIHIHIHI")
+
+print("Hello" + " " + "World")
