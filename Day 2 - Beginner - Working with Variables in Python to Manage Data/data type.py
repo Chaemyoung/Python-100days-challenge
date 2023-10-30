88b11d0cb3ecad0c294d6feb103b2fb2eab7f070
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-10-29 22:00:52 -0700

    Add files via upload

---
+++ Day 2 - Beginner - Working with Variables in Python to Manage Data/data type.py
@@ -0,0 +1,29 @@
+# String
+"Hellow"[4]
+
+print("123" + "456")
+
+# Integer
+print(123+456)
+
+# Float
+3.1443434343433434
+
+# Boolean
+True 
+False
+
+
+
+num_char = len(input("What is ur name?"))
+
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " characters.")
+
+
+# Math
+**
+*
+/
++ -
