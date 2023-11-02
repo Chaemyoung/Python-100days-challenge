2df46918bad5e19b0f6d218d2289eb64c83b41a7
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-11-01 20:59:23 -0700

    Add files via upload

---
+++ Day 4 - Beginner - Randomisation and Python Lists/list.py
@@ -0,0 +1,11 @@
+states = ["Nevada ", "Nebraska ", "Colorado ", "North Dakota ", "South Dakota ", "Montana ", "Washington ", "Idaho ", "Wyoming ", "Utah ", "Oklahoma ", "New Mexico "]
+
+states[0] = "idk "
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
