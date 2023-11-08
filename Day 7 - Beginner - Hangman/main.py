1edc8bee56b113217004757282b842b213f6819a
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-11-07 17:33:21 -0800

    Update main.py

--- Day 7 - Beginner - Hangman/main.py
+++ Day 7 - Beginner - Hangman/main.py
@@ -1,29 +1,27 @@
-word_list = ["aardvark", "baboon", "camel"]
-display = []
-# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+#Step 3
+
 import random
-random_word = random.choice(word_list)
-print(random_word)
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-# - Create an empty List called display.
-# For each letter in the chosen_word, add a "_" to 'display'.
-# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-guesses = ["_"] * len(random_word)
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-for _ in range(len(random_word)):
+#Create blanks
+display = []
+for _ in range(word_length):
     display += "_"
-print(display)
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-user_guess = input("Guess a letter!: ")
 
-# - Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for position in range(len(random_word)):
-    letter = random_word[position]
-    if letter == user_guess:
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+guess = input("Guess a letter: ").lower()
+
+#Check guessed letter
+for position in range(word_length):
+    letter = chosen_word[position]
+    print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    if letter == guess:
         display[position] = letter
 
-# - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 print(display)
