78ec82aa60bcff188e14439299ec2abddfedf60c
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-11-16 21:33:27 -0800

    Create Function.py

---
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/Function.py
@@ -0,0 +1,15 @@
+# Simple Function
+def greet():
+    print("Hellow people")
+    print("How do you do Jack Bauer?")
+    print("Isn't the weather nice today?")
+
+greet()
+
+# Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hellow{name}")
+    print(f"How do you do {name}")
+
+greet_with_name("idk")
