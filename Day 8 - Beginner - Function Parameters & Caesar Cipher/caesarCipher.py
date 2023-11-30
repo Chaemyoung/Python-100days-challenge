cc8dcfb26c54cb824ab76ec055c32bdf9ac474c7
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-11-29 21:54:42 -0800

    Update caesarCipher.py

--- Day 8 - Beginner - Function Parameters & Caesar Cipher/caesarCipher.py
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/caesarCipher.py
@@ -8,28 +8,32 @@ def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     for letter in start_text:
         position = alphabet.index(letter)
-        if cipher_direction == "encode":
-            new_position = position + shift_amount
-
-def encrypt(plain_text, shift_amount):
-    for letter in plain_text:
-        position = alphabet.index(letter)
+        if cipher_direction == "decode":
+            shift_amount *= -1
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print("The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")
 
-def decrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
-    print("The decord text is {plain_text}")
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+# def encrypt(plain_text, shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         new_letter = alphabet[new_position]
+#         cipher_text += new_letter
+#     print("The encoded text is {cipher_text}")
+
+# def decrypt(cipher_text, shift_amount):
+#     plain_text = ""
+#     for letter in cipher_text:
+#         position = alphabet.index(letter)
+#         new_position = position - shift_amount
+#         plain_text += alphabet[new_position]
+#     print("The decord text is {plain_text}")
     
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
-else:
-    pass
+# if direction == "encode":
+#     encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#     decrypt(cipher_text=text, shift_amount=shift)
