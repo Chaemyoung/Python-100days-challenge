e7df837ca4af07e62a7855f4fef0585dbaf8444f
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-12-04 21:53:49 -0800

    Update caesarCipher.py

--- Day 8 - Beginner - Function Parameters & Caesar Cipher/caesarCipher.py
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/caesarCipher.py
@@ -1,9 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     for letter in start_text:
@@ -14,7 +10,23 @@ def caesar(start_text, shift_amount, cipher_direction):
         end_text += alphabet[new_position]
     print(f"The {cipher_direction}d text is {end_text}")
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+from art import logo
+print(logo)
+
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("BYEBYE")
 
 # def encrypt(plain_text, shift_amount):
 #     cipher_text = ""
