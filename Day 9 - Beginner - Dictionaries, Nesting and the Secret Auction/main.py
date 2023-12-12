d05747af1a637d4912a86d9628559396cb057cb1
Taddii <62369103+Chaemyoung@users.noreply.github.com>
2023-12-11 21:28:10 -0800

    Update main.py

--- Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/main.py
+++ Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/main.py
@@ -20,3 +20,34 @@ print(programming_dictionary)
 for key in programming_dictionary:
     print(key)
     print(programming_dictionary[key])
+#Edit an itemin a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+print(programming_dictionary)
+
+################################################
+
+#Nesting a list in a Dictionary
+travel_log1 = {
+    "France": ["Paris", "Lille", "Digon"]
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+
+#Nesting Dictionary in a Dictionary
+Travel_log2 = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5}
+}
+
+#Nesting Dictionaries in Lists
+travel_log3 = [
+{
+    "country": "France", 
+    "cities_visited": ["Paris", "Lille", "Dijon"], 
+    "total_visits": 12
+},
+{
+    "country": "Germany",
+    "cities_visited": ["berlin", "hamburg", "stuttgart"],
+    "total_visits": 5
+}
+]
